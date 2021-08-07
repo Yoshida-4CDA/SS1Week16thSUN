bbd7bf8411ab33ff43c1824ae3dc47edbe41f63b
Yoshida-4CDA <85817467+Yoshida-4CDA@users.noreply.github.com>
2021-08-07 23:01:03 +0900

    敵の移動(反転)、乗れる足場追加(仮)

--- Assets/Yoshida/Scripts/EnemyManager.cs
+++ Assets/Yoshida/Scripts/EnemyManager.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
+    public enum DIRECTION
+    {
+        STOP,
+        RIGHT,
+        LEFT
+    }
+
+    DIRECTION direction = DIRECTION.LEFT;
+
     Rigidbody2D rb;
+    float speed;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        if (transform.localScale.x != 1)
+        {
+            direction = DIRECTION.RIGHT;
+        }
     }
 
     void Update()
@@ -17,7 +31,44 @@ public class EnemyManager : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(ParamsSO.Entity.enemySpeed * -1, rb.velocity.y);
+        switch (direction)
+        {
+            case DIRECTION.STOP:
+                speed = 0;
+                break;
+            case DIRECTION.RIGHT:
+                speed = ParamsSO.Entity.enemySpeed;
+                transform.localScale = new Vector3(-1, 1, 1);
+                break;
+            case DIRECTION.LEFT:
+                speed = ParamsSO.Entity.enemySpeed * -1;
+                transform.localScale = new Vector3(1, 1, 1);
+                break;
+        }
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Tree"))
+        {
+            ChangeDirection();
+        }
+    }
+
+    /// <summary>
+    /// à⁄ìÆï˚å¸ÇîΩì]Ç≥ÇπÇÈ
+    /// </summary>
+    void ChangeDirection()
+    {
+        if (direction == DIRECTION.RIGHT)      // âEÅÀç∂
+        {
+            direction = DIRECTION.LEFT;
+        }
+        else if (direction == DIRECTION.LEFT)  // ç∂ÅÀâE
+        {
+            direction = DIRECTION.RIGHT;
+        }
     }
 
     public void DestroyEnemy()
